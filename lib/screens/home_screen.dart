39511b196cb3d9ede3dff0fb418e51905e1df4ce
francmarin98 <francmarinc@gmail.com>
2022-05-11 08:05:43 -0500

    Adding funcionalities

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,26 +1,75 @@
 import 'package:flutter/material.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  // Propiedad
+  int counter = 10;
+
+  @override
   Widget build(BuildContext context) {
+    // Variable
     const fontSize30 = TextStyle(fontSize: 30);
 
     return Scaffold(
-        appBar: AppBar(
-          title: const Text('Home Screen'),
-          elevation: 0,
+      appBar: AppBar(
+        title: const Text('Counter Screen'),
+        elevation: 0,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            const Text('Número de clicks', style: fontSize30),
+            Text('$counter', style: fontSize30)
+          ],
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: const <Widget>[
-              Text('Número de clicks', style: fontSize30),
-              Text('0', style: fontSize30)
-            ],
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          FloatingActionButton(
+            child: const Icon(
+              Icons.exposure_minus_1_outlined,
+            ),
+            tooltip: 'Decrement',
+            onPressed: () {
+              setState(() {
+                counter++;
+              });
+            },
           ),
-        ));
+          FloatingActionButton(
+            child: const Icon(
+              Icons.exposure_zero_outlined,
+            ),
+            tooltip: 'Reset',
+            onPressed: () {
+              setState(() {
+                counter = 0;
+              });
+            },
+          ),
+          FloatingActionButton(
+            child: const Icon(
+              Icons.exposure_plus_1_outlined,
+            ),
+            tooltip: 'Increment',
+            onPressed: () {
+              setState(() {
+                counter++;
+              });
+            },
+          ),
+        ],
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+    );
   }
 }
